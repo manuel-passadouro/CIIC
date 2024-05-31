5df459decc860d113c14442cf157dd1937d2d637
Mariana Lopes Teixeira <73389688+MarianaLopesTeixeira@users.noreply.github.com>
2024-05-31 16:51:58 +0100

    Revert "Created alternative Branch for Project 2 CIIC"
    This reverts commit ec23b307a28c6611b7dc852830ae5dfa779af19b.
    
    reverted

--- project_2/distance_eval.py
+++ project_2/distance_eval.py
@@ -1,7 +1,6 @@
 import random
 import numpy as np
 import pandas as pd
-from deap import base, creator, tools
 
 # Load distance matrix from Excel file
 distance_matrix = pd.read_excel("Project2_DistancesMatrix.xlsx", index_col=0).values
@@ -18,6 +17,8 @@ ecopoints = ecopoints_df.iloc[0].tolist()
 if len(ecopoints) > 100:
     raise ValueError(f"The file contains more than 100 EcoPoints. Found {len(ecopoints)} entries.")
 
+print("EcoPoints:", ecopoints)
+
 # Define the fitness function
 def evaluate(individual):
     total_distance = 0
@@ -25,119 +26,31 @@ def evaluate(individual):
     
     # Start from the central point
     current_point = CENTRAL
-    for point in individual:
-        if num_visited >= 30 and point in [2, 42, 51, 52, 57, 68, 70, 71, 72, 73, 74, 75, 76, 77, 91]:  # adjusted for 0-indexed
-            total_distance += distance_matrix[current_point][point] * 1.4  # Apply 40% penalty
+    distances = []
+    for i in range(len(individual)):
+        next_point = individual[i]
+        if num_visited >= 30 and next_point in [2, 42, 51, 52, 57, 68, 70, 71, 72, 73, 74, 75, 76, 77, 91]:  # adjusted for 0-indexed
+            distance = distance_matrix[current_point][next_point] * 1.4  # Apply 40% penalty
         else:
-            total_distance += distance_matrix[current_point][point]
-        current_point = point
+            distance = distance_matrix[current_point][next_point]
+        distances.append(distance)
+        total_distance += distance
+        current_point = next_point
         num_visited += 1
     
     # Return to the central point
-    total_distance += distance_matrix[current_point][CENTRAL]
+    return_distance = distance_matrix[current_point][CENTRAL]
+    distances.append(return_distance)
+    total_distance += return_distance
     
-    return total_distance,
+    return distances, total_distance
 
-# Create the DEAP fitness and individual classes
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMin)
+# Generate a random path through the EcoPoints
+random_path = random.sample(ecopoints, len(ecopoints))
+print("Random Path:", random_path)
 
-# Create the DEAP toolbox
-toolbox = base.Toolbox()
+# Evaluate the random path
+distances, total_distance = evaluate(random_path)
+print("Distances between points:", distances)
+print("Total distance:", total_distance)
 
-# Register functions to create individuals and populations
-toolbox.register("indices", random.sample, range(1, len(ecopoints) + 1), len(ecopoints))
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-
-# Register the evaluation function
-toolbox.register("evaluate", evaluate)
-
-# Define the ordered crossover function for TSP
-def cxOrdered(ind1, ind2):
-    size = min(len(ind1), len(ind2))
-    cxpoint1, cxpoint2 = sorted(random.sample(range(size), 2))
-    temp1, temp2 = ind1[cxpoint1:cxpoint2+1], ind2[cxpoint1:cxpoint2+1]
-    ind1 = [x for x in ind1 if x not in temp2]
-    ind2 = [x for x in ind2 if x not in temp1]
-    ind1[cxpoint1:cxpoint2+1], ind2[cxpoint1:cxpoint2+1] = temp2, temp1
-    return ind1, ind2
-
-# Register the ordered crossover function in DEAP toolbox
-toolbox.register("mate", cxOrdered)
-
-# Register genetic operators
-toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
-
-def main():
-    random.seed(42)
-    
-    # Create an initial population of 100 individuals
-    population = toolbox.population(n=100)
-    
-    # Crossover and mutation probabilities
-    CXPB, MUTPB = 0.7, 0.2
-    
-    # Evaluate the entire population
-    fitnesses = [toolbox.evaluate(ind) for ind in population]
-    for ind, fit in zip(population, fitnesses):
-        ind.fitness.values = fit
-    
-    # Extract fitness values
-    fits = [ind.fitness.values[0] for ind in population]
-    
-    # Begin the evolution
-    g = 0
-    while g < 100:
-        g += 1
-        print("-- Generation %i --" % g)
-        
-        # Select the next generation individuals
-        offspring = toolbox.select(population, len(population))
-        
-        # Clone the selected individuals
-        offspring = list(map(toolbox.clone, offspring))
-        
-        # Apply crossover and mutation on the offspring
-        for child1, child2 in zip(offspring[::2], offspring[1::2]):
-            if random.random() < CXPB:
-                toolbox.mate(child1, child2)
-                del child1.fitness.values
-                del child2.fitness.values
-        
-        for mutant in offspring:
-            if random.random() < MUTPB:
-                toolbox.mutate(mutant)
-                del mutant.fitness.values
-        
-        # Evaluate the individuals with an invalid fitness
-        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
-        fitnesses = [toolbox.evaluate(ind) for ind in invalid_ind]
-        for ind, fit in zip(invalid_ind, fitnesses):
-            ind.fitness.values = fit
-        
-        # Replace the population by the offspring
-        population[:] = offspring
-        
-        # Gather all the fitnesses in this generation
-        fits = [ind.fitness.values[0] for ind in population]
-        
-        # Print statistics
-        length = len(population)
-        mean = sum(fits) / length
-        sum2 = sum(x*x for x in fits)
-        std = np.sqrt((sum2 / length) - (mean * mean))
-        print("  Min %s" % min(fits))
-        print("  Max %s" % max(fits))
-        print("  Avg %s" % mean)
-        print("  Std %s" % std)
-    
-    print("-- End of (successful) evolution --")
-    
-    best_ind = tools.selBest(population, 1)[0]
-    print("Best individual:", best_ind)
-    print("Best fitness:", best_ind.fitness.values[0])
-
-if __name__ == "__main__":
-    main()
